cf98999907a7829d5cb4b5d2dcc06b61f3876e0a
yash2026 <yash38709@gmail.com>
2023-12-23 00:13:36 +0530

    javascript learning

---
+++ javascript/fundamentals/challenges.js
@@ -0,0 +1,21 @@
+// const markMass = 78;
+// const johnMass = 92;
+// const markHeight = 1.69;
+// const johnHeight = 1.95;
+
+const markMass = 95;
+const johnMass = 85;
+const markHeight = 1.88;
+const johnHeight = 1.76;
+
+console.log(
+  "BMI of Mark:",
+  markMass / markHeight ** 2,
+  "BMI of John:",
+  johnMass / johnHeight ** 2
+);
+
+console.log(
+  "Mark heigher is ",
+  markMass / markHeight ** 2 > johnMass / johnHeight ** 2
+);
