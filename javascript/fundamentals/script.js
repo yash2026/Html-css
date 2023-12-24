77d94ff79254b31f26952739b04baef537b1b0a2
yash2026 <yash38709@gmail.com>
2023-12-24 23:28:45 +0530

    javascript

--- javascript/fundamentals/script.js
+++ javascript/fundamentals/script.js
@@ -1,6 +1,13 @@
 let js = "maze";
-if (js === "maze") alert("Javascript is fun");
+if (js === "maze") console.log("Javascript is fun");
 
 console.log("Yash");
 let naMe = "Yash 1";
 console.log(naMe);
+
+// text in the next line can be print in es6 using below format
+console.log(
+  "String with \n\
+multiple \n\
+line"
+);
