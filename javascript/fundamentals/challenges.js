d602249574069ec2df407857f7cf2ed645071822
yash2026 <yash38709@gmail.com>
2023-12-25 22:38:10 +0530

    type conversion

--- javascript/fundamentals/challenges.js
+++ javascript/fundamentals/challenges.js
@@ -8,14 +8,32 @@ const johnMass = 85;
 const markHeight = 1.88;
 const johnHeight = 1.76;
 
-console.log(
-  "BMI of Mark:",
-  markMass / markHeight ** 2,
-  "BMI of John:",
-  johnMass / johnHeight ** 2
-);
+const BMImark = markMass / markHeight ** 2;
+const BMIjohn = johnMass / johnHeight ** 2;
+console.log(BMImark, BMIjohn);
 
-console.log(
-  "Mark heigher is ",
-  markMass / markHeight ** 2 > johnMass / johnHeight ** 2
-);
+// console.log(
+//   "BMI of Mark:",
+//   markMass / markHeight ** 2,
+//   "BMI of John:",
+//   johnMass / johnHeight ** 2
+// );
+
+// console.log(
+//   "Mark heigher is ",
+//   markMass / markHeight ** 2 > johnMass / johnHeight ** 2
+// );
+
+if (BMImark > BMIjohn) {
+  console.log(`Mark's BMI(${BMImark}) is higher than John's BMI(${BMIjohn}).`);
+} else {
+  console.log(`John's BMI(${BMIjohn}) is higher than Mark's!(${BMImark})`);
+}
+
+const inputYear = "1991";
+console.log(inputYear + 18);
+console.log(Number(inputYear) + 18, inputYear);
+console.log("23" - "10" - 3);
+console.log("23" + "10" + 3);
+console.log("23" * "3");
+console.log("23" / "2");
--- javascript/fundamentals/script.js
+++ javascript/fundamentals/script.js
@@ -1,13 +1,20 @@
-let js = "maze";
-if (js === "maze") console.log("Javascript is fun");
+// let js = "maze";
+// if (js === "maze") console.log("Javascript is fun");
 
-console.log("Yash");
-let naMe = "Yash 1";
-console.log(naMe);
+// console.log("Yash");
+// let naMe = "Yash 1";
+// console.log(naMe);
 
-// text in the next line can be print in es6 using below format
-console.log(
-  "String with \n\
-multiple \n\
-line"
-);
+// // text in the next line can be print in es6 using below format
+// console.log(
+//   "String with \n\
+// multiple \n\
+// line"
+// );
+
+const age = 19;
+const isOldenough = age >= 18;
+
+if (isOldenough) {
+  console.log("Sarah can drive👍");
+}
