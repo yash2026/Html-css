0e3bea7a646ab3a4e3bb459e5ef410fb3f79c01f
yash2026 <yash38709@gmail.com>
2023-12-26 22:10:52 +0530

    challenge done

--- javascript/fundamentals/challenges.js
+++ javascript/fundamentals/challenges.js
@@ -3,14 +3,14 @@
 // const markHeight = 1.69;
 // const johnHeight = 1.95;
 
-const markMass = 95;
-const johnMass = 85;
-const markHeight = 1.88;
-const johnHeight = 1.76;
+// const markMass = 95;
+// const johnMass = 85;
+// const markHeight = 1.88;
+// const johnHeight = 1.76;
 
-const BMImark = markMass / markHeight ** 2;
-const BMIjohn = johnMass / johnHeight ** 2;
-console.log(BMImark, BMIjohn);
+// const BMImark = markMass / markHeight ** 2;
+// const BMIjohn = johnMass / johnHeight ** 2;
+// console.log(BMImark, BMIjohn);
 
 // console.log(
 //   "BMI of Mark:",
@@ -24,16 +24,55 @@ console.log(BMImark, BMIjohn);
 //   markMass / markHeight ** 2 > johnMass / johnHeight ** 2
 // );
 
-if (BMImark > BMIjohn) {
-  console.log(`Mark's BMI(${BMImark}) is higher than John's BMI(${BMIjohn}).`);
-} else {
-  console.log(`John's BMI(${BMIjohn}) is higher than Mark's!(${BMImark})`);
-}
+// if (BMImark > BMIjohn) {
+//   console.log(`Mark's BMI(${BMImark}) is higher than John's BMI(${BMIjohn}).`);
+// } else {
+//   console.log(`John's BMI(${BMIjohn}) is higher than Mark's!(${BMImark})`);
+// }
 
-const inputYear = "1991";
-console.log(inputYear + 18);
-console.log(Number(inputYear) + 18, inputYear);
-console.log("23" - "10" - 3);
-console.log("23" + "10" + 3);
-console.log("23" * "3");
-console.log("23" / "2");
+// const inputYear = "1991";
+// console.log(inputYear + 18);
+// console.log(Number(inputYear) + 18, inputYear);
+// console.log("23" - "10" - 3);
+// console.log("23" + "10" + 3);
+// console.log("23" * "3");
+// console.log("23" / "2");
+
+// const avgscoreDol = (96 + 108 + 89) / 3;
+// const avgscoreDol = (97 + 112 + 101) / 3;
+// const avgscoreDol = (97 + 112 + 101) / 3;
+// const avgscorekoa = (88 + 91 + 110) / 3;
+// const avgscorekoa = (109 + 95 + 123) / 3;
+// const avgscorekoa = (109 + 95 + 106) / 3;
+
+// console.log(avgscoreDol, avgscorekoa);
+// if (avgscoreDol > avgscorekoa) {
+//   console.log("Dolphin team is winner!");
+// } else if (avgscoreDol < avgscorekoa) {
+//   console.log("Koala team is winner!");
+// } else {
+//   console.log("It is draw game!");
+// }
+
+// const avgscoreDol = (96 + 108 + 89) / 3;
+// const avgscoreDol = (97 + 112 + 101) / 3;
+const avgscoreDol = (97 + 112 + 101) / 3;
+// const avgscorekoa = (88 + 91 + 110) / 3;
+// const avgscorekoa = (109 + 95 + 123) / 3;
+const avgscorekoa = (109 + 95 + 106) / 3;
+
+console.log(avgscoreDol, avgscorekoa);
+
+if (avgscoreDol > avgscorekoa && avgscoreDol >= 100) {
+  console.log("Dolphin team is winner!");
+} else if (avgscoreDol < avgscorekoa && avgscorekoa >= 100) {
+  console.log("Koala team is winner!");
+} else if (
+  avgscoreDol === avgscorekoa &&
+  avgscoreDol >= 100 &&
+  avgscorekoa >= 100
+) {
+  console.log("It is draw game!");
+} else {
+  console.log("No one wins the trophy!");
+}
